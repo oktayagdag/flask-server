b1469b734d8527db0f2ea2fab2de92b080785359
unknown <oktayyagdagg@gmaik.com>
2025-04-29 04:49:20 +0300

    server.py güncellendi: / endpoint eklendi

--- server.py
+++ server.py
@@ -1,12 +1,14 @@
 from flask import Flask, jsonify
-import os
 
 app = Flask(__name__)
 
-@app.route('/greet', methods=['GET'])
+@app.route('/')
+def home():
+    return "Server ayakta! 🚀"
+
+@app.route('/greet')
 def greet():
     return jsonify({"message": "Merhaba Oktay!"})
 
-if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host='0.0.0.0', port=port)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=10000)
